cee3a17c5bfcb662eeee5198b08f78e0c9524845
Jeroen van Maanen <jeroen@leialearns.org>
2021-02-01 10:57:53 +0100

    Replaced boilerplate code around event handler with #[dendrite_macros::event_handler].

--- src/example_event/mod.rs
+++ src/example_event/mod.rs
@@ -1,5 +1,5 @@
 use anyhow::{anyhow,Context,Result};
-use dendrite::axon_utils::{AsyncApplicableTo, AxonServerHandle, HandlerRegistry, TheHandlerRegistry, TokenStore, event_processor, empty_handler_registry};
+use dendrite::axon_utils::{AsyncApplicableTo, AxonServerHandle, TheHandlerRegistry, TokenStore, event_processor, empty_handler_registry};
 use elasticsearch::{Elasticsearch, IndexParts, GetParts};
 use log::{debug,error};
 use prost::Message;
@@ -65,64 +65,31 @@ async fn internal_process_events(axon_server_handle : AxonServerHandle) -> Resul
 
     let mut event_handler_registry: TheHandlerRegistry<ExampleQueryModel,Option<ExampleQueryModel>> = empty_handler_registry();
 
-    event_handler_registry.insert(
-        "GreetedEvent",
-        &GreetedEvent::decode,
-        &(|c, p| Box::pin(handle_event(Box::from(c), p)))
-    )?;
+    handle_greeted_event(&mut event_handler_registry)?;
 
     event_processor(axon_server_handle, query_model, event_handler_registry).await.context("Error while handling commands")
 }
 
-async fn handle_event<T: AsyncApplicableTo<P>,P: Clone>(event: Box<T>, projection: P) -> Result<()> {
-    let mut p = projection.clone();
-    event.apply_to(&mut p).await?;
-    Ok(())
-}
-
-#[tonic::async_trait]
-impl AsyncApplicableTo<ExampleQueryModel> for GreetedEvent {
-
-    async fn apply_to(self: &Self, projection: &mut ExampleQueryModel) -> Result<()> {
-        debug!("Apply greeted event to ExampleQueryModel");
-        let es_client = projection.es_client.clone();
-        if let Some(Greeting {message}) = self.message.clone() {
-            let value = message.clone();
-            let mut hasher = Sha256::new();
-            Digest::update(&mut hasher,&message);
-            let hash: Vec<u8> = hasher.finalize().to_vec();
-            let hash = base64::encode(hash);
-            let response = es_client
-                .index(IndexParts::IndexId("greetings", hash.as_str()))
-                .body(json!({
+#[dendrite_macros::event_handler]
+pub async fn handle_greeted_event(event: GreetedEvent, query_model: ExampleQueryModel) -> Result<()> {
+    debug!("Apply greeted event to ExampleQueryModel");
+    let es_client = query_model.es_client.clone();
+    if let Some(Greeting {message}) = event.message.clone() {
+        let value = message.clone();
+        let mut hasher = Sha256::new();
+        Digest::update(&mut hasher,&message);
+        let hash: Vec<u8> = hasher.finalize().to_vec();
+        let hash = base64::encode(hash);
+        let response = es_client
+            .index(IndexParts::IndexId("greetings", hash.as_str()))
+            .body(json!({
                     "id": hash,
                     "value": value,
                 }))
-                .send()
-                .await
+            .send()
+            .await
             ;
-            debug!("Elastic Search response: {:?}", response);
-        }
-        Ok(())
+        debug!("Elastic Search response: {:?}", response);
     }
-
-    fn box_clone(self: &Self) -> Box<dyn AsyncApplicableTo<ExampleQueryModel>> {
-        Box::from(GreetedEvent::clone(self))
-    }
-}
-
-pub struct ExampleQuery {
-    name: String,
-}
-
-impl ExampleQuery {
-    fn apply(self: &Self, event: GreetedEvent) -> Result<()> {
-        debug!("Apply: {:?}: {:?}", self.name, event.message);
-        Ok(())
-    }
-}
-
-#[dendrite_macros::event_handler]
-pub fn handle_greeted_event(item: GreetedEvent, context: ExampleQuery) -> Result<()> {
-    context.apply(item)
+    Ok(())
 }
